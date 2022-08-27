fcc665c7d5246aba4f03e685104f8b48476d3636
Juliana Adams <julianapadams@gmail.com>
2022-08-27 19:53:10 -0300

    Update

--- src/componentes/Rodape/Rodape.js
+++ src/componentes/Rodape/Rodape.js
@@ -5,9 +5,9 @@ export const Rodape = () => {
             <footer className="rodape">
             <ul>
                 <li>
-                <a href='#' target="_blank"><img src="/imagens/fb.png" alt="Facebook"/></a>
-                <a href='#' target="_blank"><img className='social' src="/imagens/ig.png" alt="Instagram"/></a>
-                <a href='#' target="_blank"><img  src="/imagens/tw.png" alt="Twitter"/></a>
+                <a href='https://www.facebook.com/'><img src="/imagens/fb.png" alt="Facebook"/></a>
+                <a href='https://www.instagram.com/'><img className='social' src="/imagens/ig.png" alt="Instagram"/></a>
+                <a href='https://twitter.com/'><img  src="/imagens/tw.png" alt="Twitter"/></a>
                 </li>
                 <li>
                     <img src="/imagens/logo.png" alt="Logo"/>
